bf2a94c74a899e6563c0ef70ca4b3dc7961fe252
Sergey Chivirda <si.chivirda@gmail.com>
2020-10-23 23:25:48 +0300

    feat: add styles

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import './style.css'
+import QrCode from 'qrcode'
 
-console.log('Hello awesome Webpack!')
+QrCode.toCanvas('I am a pony!', { errorCorrectionLevel: 'H', function(err, canvas) {
+  if (err) throw err
+
+  var container = document.getElementById('canvas')
+  container.appendChild(canvas)
+}})
