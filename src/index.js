c8e7a86e48e009b4ef76c2374356d3f1334fe14a
Sergey Chivirda <si.chivirda@gmail.com>
2020-10-25 20:04:53 +0300

    feat: improve dependencies for w-d-s

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import './style.css'
 import QrCode from 'qrcode'
 
-QrCode.toCanvas('I am a pony!', { errorCorrectionLevel: 'H', function(err, canvas) {
-  if (err) throw err
 
-  var container = document.getElementById('canvas')
+QrCode.toCanvas('I am a pony!', { errorCorrectionLevel: 'H', width: 400, height: 400 }, function (err, canvas) {
+  if (err) throw err
+ 
+  var container = document.getElementById('container')
   container.appendChild(canvas)
-}})
+})
