bb477fcfdf3fb14795b8e4174d5f29457a130416
Sergey Chivirda <si.chivirda@gmail.com>
2020-10-26 19:23:28 +0300

    feat:  add payment function

--- src/index.js
+++ src/index.js
@@ -2,9 +2,20 @@ import './style.css'
 import QrCode from 'qrcode'
 
 
-QrCode.toCanvas('I am a pony!', { errorCorrectionLevel: 'H', width: 400, height: 400 }, function (err, canvas) {
-  if (err) throw err
- 
-  var container = document.getElementById('container')
-  container.appendChild(canvas)
+const form = document.querySelector('.input-form')
+
+
+form.addEventListener('submit', event => {
+  event.preventDefault()
+  const reason = document.getElementById('reason').value
+  const amount = document.getElementById('amount').value
+
+  const value = `ST00012|Name=ООО Электротехснаб|PersonalAcc=40702810804000006070|BankName=АРХАНГЕЛЬСКОЕ ОТДЕЛЕНИЕ N 8637 ПАО СБЕРБАНК|BIC=041117601|CorrespAcc=30101810100000000601|Purpose=${reason}|Sum=${amount}|PayeeINN=3525394791|KPP=290143001`
+
+  QrCode.toCanvas(value, { errorCorrectionLevel: 'H', width: 300, height: 300 }, function (err, canvas) {
+    if (err) throw err
+   
+    var container = document.getElementById('container')
+    container.appendChild(canvas)
+  })
 })
