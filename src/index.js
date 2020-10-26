c55d2311c10921c10bcfcb0134fe58959a091617
Sergey Chivirda <si.chivirda@gmail.com>
2020-10-26 22:06:53 +0300

    feat: add stiles, add check canvas container

--- src/index.js
+++ src/index.js
@@ -9,13 +9,17 @@ form.addEventListener('submit', event => {
   event.preventDefault()
   const reason = document.getElementById('reason').value
   const amount = document.getElementById('amount').value
+  const container = document.getElementById('container')
+
+  if (container.hasChildNodes()) {
+    container.removeChild()
+  }
 
   const value = `ST00012|Name=ООО Электротехснаб|PersonalAcc=40702810804000006070|BankName=АРХАНГЕЛЬСКОЕ ОТДЕЛЕНИЕ N 8637 ПАО СБЕРБАНК|BIC=041117601|CorrespAcc=30101810100000000601|Purpose=${reason}|Sum=${amount}|PayeeINN=3525394791|KPP=290143001`
 
-  QrCode.toCanvas(value, { errorCorrectionLevel: 'H', width: 300, height: 300 }, function (err, canvas) {
+  QrCode.toCanvas(container, value, { errorCorrectionLevel: 'Q', width: 300, height: 300 }, function (err, canvas) {
     if (err) throw err
    
-    var container = document.getElementById('container')
-    container.appendChild(canvas)
+    container.append(canvas)
   })
 })
